4138cb6b993b0eac8719c50dc8615429c0491520
Muftobration <Muftobration@340f0021-ae93-2407-846b-9c971d96242e>
2010-10-26 03:05:57 +0000

    Changed Node.h and modified Final_MazeGame.cxx to reflect the changes. The nested loop in printMaze needs to be finished.

--- trunk/Node.h
+++ trunk/Node.h
@@ -1,18 +1,37 @@
 #include <math.h>
+#define N 1
+#define S 2
+#define E 4
+#define W 8
+
 class Node {
 private:
 	int _x,_y;
 	bool _visited;
-	Node * _edge[4];
-	int _edges;
+	unsigned int _openEdges;
 public:
-	Node() {_edges = 0; _visited = false;}
-	Node(int x, int y) {_x=x; _y=y; _edges = 0; _visited=false;}
+	Node() {_x = 0; _y = 0; _openEdges = 0; _visited = false;}
+	Node(int x, int y) {_x = x; _y = y; _openEdges = 0; _visited = false;}
 	int getX() {return _x;}
 	int getY() {return _y;}
-	void addEdge(Node * edge) {if(_edges < 4){_edge[_edges]=edge; _edges++;}}
+	void addEdge(unsigned int openEdges) {_openEdges = openEdges;}
 	void visit() {_visited = true;}
 	void unvisit() {_visited = false;}
-	Node ** getEdges() {return _edge;}
-	int getEdgeSize() {return _edges;}
+	unsigned int getEdges() {return _openEdges;}
+	bool youCanGoNorth()
+	{
+	    if (_openEdges == 1 || _openEdges == 3 || _openEdges == 5 ||
+	        _openEdges == 9 || _openEdges == 7 || _openEdges == 11 || _openEdges == 13)
+	        return true;
+        else
+	        return false;
+	}
+    bool youCanGoWest()
+	{
+	    if (_openEdges == 8 || _openEdges == 9 || _openEdges == 10 ||
+	        _openEdges == 12 || _openEdges == 11 || _openEdges == 13 || _openEdges == 14)
+	        return true;
+        else
+	        return false;
+	}
 };
