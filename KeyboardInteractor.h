79319c6f132f9f46335180482a553098695e5032
Muftobration <Muftobration@340f0021-ae93-2407-846b-9c971d96242e>
2010-12-04 04:41:38 +0000

    Rigged-position polyhedra at (1, 0) and (0, 1). Need to do animation.

--- KeyboardInteractor.h
+++ KeyboardInteractor.h
@@ -2,6 +2,7 @@
 #define _KEYBOARDINTERACTOR_H
 
 #include <vector>
+#include <iostream>
 
 #include "Node.h"
 
@@ -13,7 +14,7 @@
 #define COLLISIONBUFFER .4
 
 // Define interaction style
-class KeyPressInteractorStyle : public vtkInteractorStyleTrackballCamera
+class KeyPressInteractorStyle : public vtkInteractorStyle/*vtkInteractorStyleTrackballCamera*/
 {
   private:
 	vtkSmartPointer<vtkCamera> Camera;
@@ -49,13 +50,6 @@ class KeyPressInteractorStyle : public vtkInteractorStyleTrackballCamera
         double yLook = yFocus - yPosition;
         double angle;
 
-//////
-
-        // Calculate the new position we will be at after moving
-
-        // If the new position is in a valid region for that node, move there
-
-
         // Be careful not to divide by zero! If x is close enough to zero, we will just call it zero
         // and set the angle to Pi/2.
         if (fabs(xLook) > 0.0001)
@@ -71,13 +65,13 @@ class KeyPressInteractorStyle : public vtkInteractorStyleTrackballCamera
         {
             angle = PI + angle;
         }
-        /*
-        std::cout << "BEFORE MOVEMENT\n";
+
+        //std::cout << "BEFORE MOVEMENT\n";
         std::cout << "Camera position: (" << xPosition << ", " << yPosition << ")." << std::endl;
-        std::cout << "Focal position:  (" << xFocus << ", " << yFocus << ")." << std::endl;
-        std::cout << "Look direction:  (" << xLook << ", " << yLook << ")." << std::endl;
-        std::cout << "Angle:           " << angle << "\n" <<std::endl;
-        */
+        //std::cout << "Focal position:  (" << xFocus << ", " << yFocus << ")." << std::endl;
+        //std::cout << "Look direction:  (" << xLook << ", " << yLook << ")." << std::endl;
+        //std::cout << "Angle:           " << angle << "\n" <<std::endl;
+
         // Handles the arrow keys and moves the Player accordingly
         if(key.compare("Up") == 0)
         {
@@ -86,14 +80,14 @@ class KeyPressInteractorStyle : public vtkInteractorStyleTrackballCamera
             int row = -yPosition;
             int col = xPosition;
             if(!tempMaze[row][col]->youCanGoWest() && newX < col - COLLISIONBUFFER)
-		newX = col - COLLISIONBUFFER;
+                newX = col - COLLISIONBUFFER;
             else if(!tempMaze[row][col]->youCanGoEast() && newX > col + COLLISIONBUFFER)
-		newX = col + COLLISIONBUFFER;
+                newX = col + COLLISIONBUFFER;
             if(!tempMaze[row][col]->youCanGoNorth() && newY > -row + COLLISIONBUFFER)
-		newY = -row + COLLISIONBUFFER;
+                newY = -row + COLLISIONBUFFER;
             else if(!tempMaze[row][col]->youCanGoSouth() && newY < -row - .4)
-		newY = -row - COLLISIONBUFFER;
-            
+                newY = -row - COLLISIONBUFFER;
+
             Camera->SetPosition(newX, newY, 0);
         }
         if(key.compare("Down") == 0)
@@ -103,14 +97,14 @@ class KeyPressInteractorStyle : public vtkInteractorStyleTrackballCamera
             int row = -yPosition;
             int col = xPosition;
             if(!tempMaze[row][col]->youCanGoWest() && newX < col - COLLISIONBUFFER)
-		newX = col - COLLISIONBUFFER;
+                newX = col - COLLISIONBUFFER;
             else if(!tempMaze[row][col]->youCanGoEast() && newX > col + COLLISIONBUFFER)
-		newX = col + COLLISIONBUFFER;
+                newX = col + COLLISIONBUFFER;
             if(!tempMaze[row][col]->youCanGoNorth() && newY > -row + COLLISIONBUFFER)
-		newY = -row + COLLISIONBUFFER;
+                newY = -row + COLLISIONBUFFER;
             else if(!tempMaze[row][col]->youCanGoSouth() && newY < -row - COLLISIONBUFFER)
-		newY = -row - COLLISIONBUFFER;
-            
+                newY = -row - COLLISIONBUFFER;
+
             Camera->SetPosition(newX, newY, 0);
         }
         if(key.compare("Right") == 0)
@@ -142,9 +136,34 @@ class KeyPressInteractorStyle : public vtkInteractorStyleTrackballCamera
         int col = xPosition;
         std::cout << "X: " << xPosition << " Y: " << yPosition << std::endl;
         std::cout << "col: " << tempMaze[row][col]->getX() << " row: " << tempMaze[row][col]->getY() << std::endl;
-*/	
-        this->Interactor->GetRenderWindow()->Render();
+*/
 
+        // A test for camera roll
+        if (key.compare("h") == 0/* && Camera->GetRoll() == 0*/)
+        {
+            Camera->Roll(180);
+        }
+        /*else if (key.compare("P") == 0 && Camera->GetRoll() == PI)
+        {
+            Camera->SetRoll(0);
+        }*/
+
+        this->Interactor->GetRenderWindow()->Render();
+    }
+
+    virtual void OnKeyDown()
+    {
+        //std::cout << "Key Down" << std::endl;
+    }
+
+    virtual void OnKeyUp()
+    {
+        //std::cout << "Key Up" << std::endl;
+    }
+
+    virtual void OnKeyRelease()
+    {
+        //std::cout << "Key Release" << std::endl;
     }
 };
 
